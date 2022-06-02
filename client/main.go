f1ebe4108c8a1f1cde7f2738d8a3958f75b5e493
Vikash Parashar <99866771+gowithvikash@users.noreply.github.com>
2022-06-02 15:38:45 +0530

    some more changes

--- client/main.go
+++ client/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	pb "github.com/gowithvikash/grpc_with_go/greet/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 // import (
-// 	pb "github.com/gowithvikash/grpc_with_go/greet/proto"
 // )
 
 var (
@@ -11,23 +18,30 @@ var (
 )
 
 func main() {
-	fmt.Println("Client Is Trying To Connect With Server On Address : ", address)
+	fmt.Println("__Client Is Trying To Connect With Server On Address : ", address)
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("__Client Is Failed To Connect With Server !")
+	}
+	fmt.Println("__Client Is Successfully Connected To Server . ")
+	defer conn.Close()
+	c := pb.NewGreetServiceClient(conn)
 
-	do_Simple_Greet()
-	do_Greet_Many_Times()
-	do_Long_Greet()
-	do_Greet_Every_One()
+	do_Simple_Greet(c)
+	// do_Greet_Many_Times(c)
+	// do_Long_Greet(c)
+	// do_Greet_Every_One(c)
 }
 
-func do_Simple_Greet() {
-	fmt.Println("-------- do_Simple_Greet() Function Was Invoked At Client --------")
+func do_Simple_Greet(c pb.GreetServiceClient) {
+	fmt.Println("_______   do_Simple_Greet() Function Was Invoked At Client   _______")
 }
-func do_Greet_Many_Times() {
-	fmt.Println("-------- do_Greet_Many_Times() Function Was Invoked At Client --------")
+func do_Greet_Many_Times(c pb.GreetServiceClient) {
+	fmt.Println("_______ do_Greet_Many_Times() Function Was Invoked At Client _______")
 }
-func do_Long_Greet() {
-	fmt.Println("-------- do_Long_Greet() Function Was Invoked At Client --------")
+func do_Long_Greet(c pb.GreetServiceClient) {
+	fmt.Println("_______    do_Long_Greet() Function Was Invoked At Client    _______")
 }
-func do_Greet_Every_One() {
-	fmt.Println("-------- do_Greet_Every_One() Function Was Invoked At Client --------")
+func do_Greet_Every_One(c pb.GreetServiceClient) {
+	fmt.Println("_______ do_Greet_Every_One()  Function Was Invoked At Client _______")
 }
--- server/main.go
+++ server/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
+	pb "github.com/gowithvikash/grpc_with_go/greet/proto"
 	"google.golang.org/grpc"
-	// pb "github.com/gowithvikash/grpc_with_go/greet/proto"
 )
 
 var (
@@ -15,32 +16,36 @@ var (
 )
 
 type Server struct {
+	pb.GreetServiceServer
 }
 
 func main() {
-	fmt.Println("Server Is Trying To Get Ready ! Once Done , Client Can Connect With Server And Can Send Requests To Get Response .")
+	fmt.Println("__Server Is Trying To Get Ready ! Once Done , Client Can Connect With Server And Can Send Requests To Get Response .")
 	lis, err := net.Listen(network, address)
 	if err != nil {
-		log.Fatal("Server Listning Error !")
+		log.Fatal("__Server Listning Error !")
 	}
-	fmt.Println("Server Is Listning On Address : ", address, " Successfully .")
+	fmt.Println("__Server Is Listning On Address : ", address, " Successfully .")
 	s := grpc.NewServer()
+	pb.RegisterGreetServiceServer(s, &Server{})
 	if err = s.Serve(lis); err != nil {
-		log.Fatal("Server Serving Failed !")
+		log.Fatal("__Server Serving Failed !")
 	}
-	fmt.Println("Server Is Ready To Server On Address : ", address)
+	fmt.Println("__Server Is Ready To Server On Address : ", address)
 
 }
 
-func (s *Server) Simple_Greet() {
+func (s *Server) Simple_Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
 	fmt.Println("_____________   Simple_Greet() Function Was Invoked At Server   _____________")
+	return &pb.GreetResponse{Result: "Hello , " + in.Name + " ! "}, nil
 }
-func (s *Server) Greet_Many_Times() {
-	fmt.Println("___________   Greet_Many_Times() Function Was Invoked At Server   ___________")
-}
-func (s *Server) Long_Greet() {
-	fmt.Println("____________     Long_Greet() Function Was Invoked At Server    _____________")
-}
-func (s *Server) Greet_Every_One() {
-	fmt.Println("____________  Greet_Every_One() Function Was Invoked At Server  _____________")
-}
+
+// func (s *Server) Greet_Many_Times(in *pb.GreetRequest, stream pb.GreetService_Greet_Many_TimesServer) error {
+// 	fmt.Println("___________   Greet_Many_Times() Function Was Invoked At Server   ___________")
+// }
+// func (s *Server) Long_Greet(stream pb.GreetService_Long_GreetServer) error {
+// 	fmt.Println("____________     Long_Greet() Function Was Invoked At Server    _____________")
+// }
+// func (s *Server) Greet_Every_One(stream pb.GreetService_Greet_Every_OneServer) error {
+// 	fmt.Println("____________  Greet_Every_One() Function Was Invoked At Server  _____________")
+// }
